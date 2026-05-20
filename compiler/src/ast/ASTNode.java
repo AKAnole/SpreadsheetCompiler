0155f48348b1be592756532c8ac520f2b8b16c44
AKAnole <103553176+AKAnole@users.noreply.github.com>
2026-05-20 23:26:02 +0300

    Delete compiler directory

--- compiler/src/ast/ASTNode.java
+++
@@ -1,9 +0,0 @@
-package ast;
-
-public abstract class ASTNode {
-    public abstract String toTree(String prefix);
-    @Override
-    public String toString() {
-        return toTree("");
-    }
-}
